4bc5ead72db866f2e72cd548ff0df4f006d74e12
zhenn <example@gmail.com>
2021-11-17 14:55:46 +0300

    second

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,16 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow:  '<button type="button" class="slick-prev"><img src"icons/left.png"></button>',
-        nextArrow:  '<button type="button" class="slick-next"><img src"icons/right.png"></button>'
+        prevArrow:  '<button type="button" class="slick-prev"><img src"icons/left.svg"></button>',
+        nextArrow:  '<button type="button" class="slick-next"><img src"icons/right.svg"></button>',
+        respobsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                } 
+            }
+        ]
     });
   });
