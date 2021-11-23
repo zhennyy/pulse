f5837527f0c98d13723c3c61a4da9bf4a69b3840
zhenn <example@gmail.com>
2021-11-23 19:36:59 +0300

    live open

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow:  '<button type="button" class="slick-prev"><img src"icons/left.svg"></button>',
-        nextArrow:  '<button type="button" class="slick-next"><img src"icons/right.svg"></button>',
+        prevArrow:  '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow:  '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         respobsive: [
             {
                 breakpoint: 992,
@@ -82,4 +82,20 @@ $(document).ready(function(){
     valideForms("#order form");
 
     $("input[name=phone]").mask("+7 (999) 999-99-99");
+
+    $("form").submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $("#consultation, #order").fadeOut();
+            $(".overlay, #thanks").fadeIn("slow");
+            
+            $("form").trigger("reset");
+        });
+        return false;
+    });
 });
